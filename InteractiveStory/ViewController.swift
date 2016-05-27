539baf962b23ba00ce29b2c7ec1d73c417d37de7
jwaggoner <jwaggoner@joshs-macbook-pro.local>
2016-05-27 14:44:43 -0400

    Initial Commit

---
+++ InteractiveStory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InteractiveStory
+//
+//  Created by Josh Waggoner on 5/27/16.
+//  Copyright © 2016 60watts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
