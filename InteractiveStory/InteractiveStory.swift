4b678a81d4b362796860a9d5e18c3c182c1197b7
Jwaggoner <wiggiddywags@gmail.com>
2016-05-28 12:59:00 -0400

    added InteractiveStory file

---
+++ InteractiveStory/InteractiveStory.swift
@@ -0,0 +1,22 @@
+//
+//  InteractiveStory.swift
+//  InteractiveStory
+//
+//  Created by Josh Waggoner on 5/28/16.
+//  Copyright © 2016 60watts. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+enum Story {
+    case ReturnTrip
+    case TouchDown
+    case Homeward
+    case Rover
+    case Cave
+    case Crate
+    case Monster
+    case Droid
+    case Home
+}
