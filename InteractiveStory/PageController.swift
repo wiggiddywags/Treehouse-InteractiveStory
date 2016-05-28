0352bc1c5186133b169e5391e2e4fc104d7ccd11
Jwaggoner <wiggiddywags@gmail.com>
2016-05-28 13:53:43 -0400

    init PageController.swift

---
+++ InteractiveStory/PageController.swift
@@ -0,0 +1,48 @@
+//
+//  PageController.swift
+//  InteractiveStory
+//
+//  Created by Josh Waggoner on 5/28/16.
+//  Copyright © 2016 60watts. All rights reserved.
+//
+
+import UIKit
+
+class PageController: UIViewController {
+    
+    var page : Page?
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    init(page: Page) {
+        self.page = page
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .blueColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
